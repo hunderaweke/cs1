dc13152cbbc85927d535078fec59b63217aba096
hunderaweke@gmail.com <hunderaweke@gmail.com>
2023-03-31 05:33:04 +0000

    Updated and solved some problems

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cs1-robots",
-    version='0.1.1',
+    version='0.1.6',
     author='David Letscher and Michael H. Goldwasser',
     author_email='',
     packages=find_packages('src'),
