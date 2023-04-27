bf200c3153a1407c51334b747aa1b18167c8a216
hunderaweke@gmail.com <hunderaweke@gmail.com>
2023-04-27 04:32:44 +0000

    Updated: README.md,setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
     name="cs1-robots",
     version='0.1.6.2',
     author='David Letscher and Michael H. Goldwasser',
-    author_email='',
     packages=find_packages('src'),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_dir={'': 'src'},
-    url='',
+    url='https://github.com/hunderaweke/cs1',
+    author_email="hunderaweke@gmail.com",
     keywords='cs1robots',
     install_requires=[
         'pillow',
